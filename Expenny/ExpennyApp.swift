05e39e87b8b1b5b8bf88a3da2a5333f9c4b94f51
Ladislav Szolik <szolik.ladislav@gmail.com>
2023-01-15 13:34:40 +0100

    Initial Commit

---
+++ Expenny/ExpennyApp.swift
@@ -0,0 +1,20 @@
+//
+//  ExpennyApp.swift
+//  Expenny
+//
+//  Created by Ladislav Szolik on 15.01.23.
+//
+
+import SwiftUI
+
+@main
+struct ExpennyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
